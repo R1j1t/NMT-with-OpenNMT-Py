962bc12717f557bc857a4e7243823cc21a004293
R1j1t <rajat.goel@iitgn.ac.in>
2018-10-21 23:07:31 +0530

    Updated the script for 3 outputs

--- dataset_split.py
+++ dataset_split.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import numpy as np
 import csv
+from subprocess import Popen, PIPE
 
-with open('./europarl-v7.fr-en.fr', 'r') as file:
+
+# TODO: Correct the output, has `"` as the starting character
+
+#path = input('Enter the directory in which Fr and En txt files are:')
+
+with open('/Users/rajat/Downloads/fr-en/europarl-v7.fr-en.fr', 'r') as file:
 	fr_snts = file.readlines()
 
 	## Mapping the sentences with the help of a dataframe
 	dataset = pd.DataFrame(np.nan,index=range(len(fr_snts)), columns=['fr','en'])
 	dataset['fr'] = fr_snts
 
-with open ('./europarl-v7.fr-en.en') as enFile:
+with open ('{0}/europarl-v7.fr-en.en'.format(path)) as enFile:
 	dataset.en = enFile.readlines()
 
 ## Shuffling the dataset
@@ -24,4 +30,17 @@ train = dataset.iloc[:train_marker,:]
 valid = dataset.iloc[train_marker:val_marker,:]
 test  = dataset.iloc[val_marker:test_marker,:]
 #print(train.head())
-train.to_csv('/Users/rajat/Downloads/fr-en/train.en',line_terminator='', columns=['fr'],header=False,index=False,sep=' ',encoding='utf-8')
+
+## Output files
+train.to_csv('./data/fr-en//train.fr',line_terminator='', columns=['fr'],header=False,index=False,sep=' ',encoding='utf-8')
+train.to_csv('./data/fr-en//train.en',line_terminator='', columns=['en'],header=False,index=False,sep=' ',encoding='utf-8')
+valid.to_csv('./data/fr-en//train.fr',line_terminator='', columns=['fr'],header=False,index=False,sep=' ',encoding='utf-8')
+valid.to_csv('./data/fr-en//train.en',line_terminator='', columns=['en'],header=False,index=False,sep=' ',encoding='utf-8')
+test.to_csv('./data/fr-en//train.fr',line_terminator='', columns=['fr'],header=False,index=False,sep=' ',encoding='utf-8')
+test.to_csv('./data/fr-en//train.en',line_terminator='', columns=['en'],header=False,index=False,sep=' ',encoding='utf-8')
+
+## CHECK: Try to run the perl scripts from python
+for j in ['train','valid','test']:
+	for i in ['en','fr']:
+		var = r'./data/{}.{}.atok'.format(j,i)
+		print('perl ../tools/tokenizer.perl -a -no-escape -l {0} < {1}.{2} > {3}.{4}.atok'.format(i,j,i,j,i))
