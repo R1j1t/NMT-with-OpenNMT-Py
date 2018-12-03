c01eaf47f4d12ca73a4cd8bbd74a1f5bf7faa48d
R1j1t <rajat.goel@iitgn.ac.in>
2018-12-03 12:12:35 +0530

    Updated comments

--- dataset_split.py
+++ dataset_split.py
@@ -6,6 +6,7 @@ from subprocess import Popen, PIPE
 
 # TODO: Correct the output, has `"` as the starting character
 
+# Input directory path to the french and english files
 path = input('Enter the directory in which Fr and En txt files are:')
 
 with open('{0}/europarl-v7.fr-en.fr'.format(path), 'r') as file:
@@ -20,18 +21,20 @@ with open ('{0}/europarl-v7.fr-en.en'.format(path)) as enFile:
 
 ## Shuffling the dataset
 dataset = dataset.sample(frac=1)
-#print(dataset.head())
-#print()
+
+# Creating markers for help in dataset split
 train_marker = int(0.85*len(fr_snts))
 val_marker = int(0.90*len(fr_snts))
 test_marker = int(1.00 *len(fr_snts))
 
+# Segmented the dataset into train Valid and test set using markers created above 
 train = dataset.iloc[:train_marker,:]
 valid = dataset.iloc[train_marker:val_marker,:]
 test  = dataset.iloc[val_marker:test_marker,:]
 #print(train.head())
 
-## Output files
+## Output seprate files for 2 languages and 3 splits
+
 train.to_csv('../data/fr-en/train.fr',line_terminator='', columns=['fr'],header=False,index=False,sep=' ',encoding='utf-8')
 train.to_csv('../data/fr-en/train.en',line_terminator='', columns=['en'],header=False,index=False,sep=' ',encoding='utf-8')
 print('Training File Exported')
